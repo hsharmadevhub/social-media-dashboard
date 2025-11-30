84caa34fbdf82cbbfadb9a7239477f05f8bc235d
Harsh Sharma <36968863+hsharmadrexel@users.noreply.github.com>
2025-11-30 16:10:11 +0530

    Register routes for posts resource

---
+++ src/modules/posts/posts.routes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const postRoutes = express.Router();
+
+postRoutes.get('');
+postRoutes.post('');
+postRoutes.get('/:id');
+postRoutes.put('/:id');
+postRoutes.delete('/:id');
